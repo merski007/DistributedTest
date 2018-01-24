2e7c4201dc8cdd97d1bfb3cd45c46b570aae9ba7
mjmersenski <mjmersenski@mflaptop01.qg.com>
2018-01-23 19:00:59 -0600

    added a second line of text to the startup class

--- src/test/startup.java
+++ src/test/startup.java
@@ -7,5 +7,6 @@ package test;
 public class startup {
     public static void main(String[] args) {
         System.out.println("TEST");
+        System.out.println("Updating code to test git push");
     }
 }
