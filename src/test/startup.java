2bcae761bfe7b705799f7229e4cd135f3584674f
mjmersenski <mjmersenski@mflaptop01.qg.com>
2018-01-23 19:08:21 -0600

    added a third line of text to the startup class to test multiple commits and then 1 push to git

--- src/test/startup.java
+++ src/test/startup.java
@@ -8,5 +8,7 @@ public class startup {
     public static void main(String[] args) {
         System.out.println("TEST");
         System.out.println("Updating code to test git push");
+        
+        System.out.println("Testing multiple commits with 1 git add");
     }
 }
